2dcea811f21ec6606c5daaddb4ca95fcf62eca8f
Melissa Wheatley <melissa.c.wheatley@gmail.com>
2018-04-01 19:44:37 -0500

    moved login functions and listeners out of main and into user.js and added logout ID to logout element

--- javascript/user.js
+++ javascript/user.js
@@ -52,5 +52,40 @@ function addUser(userObj) {
    });
 }
 
+//BUILD USER OBJECT
+function buildUserObj(uid, email, name) {
+    let userObj = {
+        uid: uid,
+        email: email,
+        fullName: name,
+        userTypeID: null
+  };
+  console.log("userObj:", userObj);
+  return userObj;
+}
+
+//SEND INFO TO FIREBASE
+function sendToFirebase(userObj){
+	addUser(userObj);
+}
+
+//EVENT LISTENERS
+//LOGIN
+$("#login").click(function(){
+    // console.log("user clicked login");
+    googleLogIn()
+    .then((result) => {
+        let currentUser = result;
+        // console.log("UID result from login: ", result.user.uid);
+        setUser(result.user.uid);
+        $("#login").addClass("d-none");
+        $("#userPic").removeClass("d-none").html(`${result.user.displayName}  <img src="${result.user.photoURL}" alt="${result.user.displayName} photo from Google" class="profPic rounded-circle">`);
+        sendToFirebase(buildUserObj(result.user.uid, result.user.email, result.user.displayName));
+        // console.log("login complete!");
+        return currentUser;
+    });
+    console.log("UID result from login: ", currentUser.user.uid);
+});
+
 
 module.exports = {googleLogIn, googleLogOut, setUser, getUser, addUser};
